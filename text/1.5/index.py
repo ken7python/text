49400fe70b0e0b2e3790c6ce6d4aa73a47fe0850
ken7python <66853112+ken7python@users.noreply.github.com>
2020-09-24 17:53:25 +0900

    Add files via upload

---
+++ text/1.5/index.py
@@ -0,0 +1,141 @@
+n = ""
+import subprocess
+import wx
+import os
+import time
+import tkinter,tkinter.filedialog,tkinter.messagebox
+import codecs
+import chardet
+import sys
+
+# -*- coding: utf-8 -*-
+global hifa
+global kekka
+global er
+er = 0
+hifa = ""
+app = wx.App(False)
+frame = wx.Frame(None,-1,"テキストエディタ",size=(1000,900))
+panel = wx.Panel(frame,wx.ID_ANY)
+
+fi = wx.TextCtrl(panel,-1,pos=(200,50),size=(800,50))
+fi.Disable()
+fontfi = wx.Font(15, wx.FONTFAMILY_DEFAULT, 
+               wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+fi.SetFont(fontfi)
+
+
+text = wx.TextCtrl(panel,pos=(200,100),size=(800,690), style=wx.TE_MULTILINE)
+global koma
+koma = wx.TextCtrl(panel,pos=(0,150),size=(200,400), style=wx.TE_MULTILINE)
+koma.Disable()
+save_button = wx.Button(panel, -1, pos=(10, 10), label='保存')
+ofa_button = wx.Button(panel, -1, pos=(10, 50), label='ファイルを開く')
+zikkou_button = wx.Button(panel, -1, pos=(10, 80), label='実行')
+kou = wx.Button(panel,pos=(100,50),label="最新表示")
+
+font = wx.Font(25, wx.FONTFAMILY_DEFAULT, 
+               wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+text.SetFont(font)
+text.SetForegroundColour("#ffffff")
+text.SetBackgroundColour('#333333')
+
+def get_kousin(event):
+    ls_file_name = os.listdir() 
+    fi.Clear()
+    for i in range(len(ls_file_name)):
+        fi.AppendText(ls_file_name[i]+"  ")
+
+def get_file():
+    ls_file_name = os.listdir() 
+    fi.Clear()
+    for i in range(len(ls_file_name)):
+        fi.AppendText(ls_file_name[i]+"  ")
+def click_save_button(event):  # 保存ボタンをクリック時の動作
+    if not hifa == "":
+        data = codecs.open(hifa, "w" , hc)
+        data.write(text.GetValue())
+        data.close()
+        get_file()
+    else:
+        tkinter.messagebox.showinfo("テキストエディタ","ファイルを開いていません")
+def faop(ofa):
+    # ファイルをオープンする
+    global hifa
+    hifa = ofa
+    os.chdir(str(os.path.dirname(os.path.abspath(hifa))))
+    datak = codecs.open(hifa, "rb")
+    data = codecs.open(hifa,"rb",chardet.detect(datak.read())['encoding'])
+    global hc
+    hc = chardet.detect(datak.read())['encoding']
+    # すべての内容を読み込む
+    contents = data.read()
+
+    # すべての内容を表示する
+    text.SetValue(contents)
+
+    #ファイルをクローズする
+    datak.close()
+    data.close()
+    fi.SetValue(hifa)
+def fileopen(event):
+    fTyp = [("","*")]
+    iDir = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Desktop"
+    fl = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+    faop(fl)
+def komando(k):
+    er = 0
+    result = subprocess.run([k,str(os.path.split(hifa)[1])],stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='shift_jis')
+    if result.stderr == "":
+        er = 0
+        kekka = result.stdout
+        if str(os.path.splitext(hifa)[1]) == ".java":
+            name = os.path.splitext(os.path.basename(os.path.split(hifa)[1]))[0]
+            result = subprocess.run(['java',name], stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='shift_jis')
+            if not result.stderr == "":
+                er = 1
+                kekka = result.stderr
+            koma.SetValue(kekka)
+        else:
+            name = str(os.path.split(hifa)[1])
+        if er == 0:
+            kekka = result.stdout
+            koma.SetValue(kekka)
+    else:
+        kekka = result.stderr
+        print(kekka)
+        koma.SetValue(kekka)
+    
+def zi(event):
+    if str(os.path.splitext(hifa)[1]) == ".java":
+        os.chdir(str(os.path.dirname(os.path.abspath(hifa))))
+        komando("javac")
+    if str(os.path.splitext(hifa)[1]) == ".py":
+        os.chdir(str(os.path.dirname(os.path.abspath(hifa))))
+        komando("python")
+    if str(os.path.splitext(hifa)[1]) == ".htm":
+        os.chdir(str(os.path.dirname(os.path.abspath(hifa))))
+        subprocess.Popen(['start',str(hifa)], shell=True)
+    if str(os.path.splitext(hifa)[1]) == ".html":
+        os.chdir(str(os.path.dirname(os.path.abspath(hifa))))
+        subprocess.Popen(['start',str(hifa)], shell=True)
+
+
+
+get_file()
+
+save_button.Bind(wx.EVT_BUTTON, click_save_button)
+kou.Bind(wx.EVT_BUTTON, get_kousin)
+zikkou_button.Bind(wx.EVT_BUTTON, zi)
+ofa_button.Bind(wx.EVT_BUTTON, fileopen)
+
+
+hn = len(sys.argv)
+if (hn > 1):
+    faop(sys.argv[1])
+
+
+
+frame.Show()
+    
+app.MainLoop()
